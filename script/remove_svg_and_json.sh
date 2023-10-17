dfb0f63985c73bd7c816215c902ee55751453430
xiaofengli <cs.fenglixiao@gmail.com>
2023-10-17 21:50:23 +0800

    add shell to remove svg

---
+++ script/remove_svg_and_json.sh
@@ -0,0 +1,4 @@
+rm -f /home/xfl/pyCode/GFVis/static/image/svg/acl/*
+rm -f /home/xfl/pyCode/GFVis/static/image/svg/visualization/*
+rm -f /home/xfl/pyCode/GFVis/static/json/acl/*
+rm -f /home/xfl/pyCode/GFVis/static/json/visualization/*
